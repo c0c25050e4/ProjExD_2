1abf4524ea337d16b1975b34f90c5bc9f3776d73
c0c25050e4 <c0c25050e4@edu.teu.ac.jp>
2026-06-16 14:52:41 +0900

    練習４

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,30 +1,60 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
-DELTA = {pg.K_UP: (0,-5),
-         pg.K_DOWN:(0,+5),
-         pg.K_LEFT: (-5,0),
-         pg.K_RIGHT: (+5,0),
+DELTA = {
+    pg.K_UP: (0, -5),  # 上矢印キー
+    pg.K_DOWN: (0, +5),  # 下矢印キー
+    pg.K_LEFT: (-5, 0),  # 左矢印キー
+    pg.K_RIGHT: (+5, 0),  # 右矢印キー
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or 爆弾Rect
+    戻り値：判定結果タプル（横方向判定結果，縦方向判定結果）
+    True：画面内／False：画面外
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
+    # こうかとんの初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    # 爆弾の初期化
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 横初期座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 縦初期座標
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾Rectが重なったら
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -37,12 +67,22 @@ def main():
         #     sum_mv[0] -= 5
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
-        for key,mv in DELTA.items():
+        for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0] #横方向の移動量
-                sum_mv[1] += mv[1] #縦方向の移動量
+                sum_mv[0] += mv[0]  # 横方向の移動量
+                sum_mv[1] += mv[1]  # 縦方向の移動量
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 動きをなかったことにする
         screen.blit(kk_img, kk_rct)
+
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
